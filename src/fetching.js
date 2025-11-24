e8d7cf474ac0010148840ec1388b0aa7f5737b26
Stephan <stephan_habermacher@gmx.ch>
2025-11-24 22:42:38 +0100

    Implement Current Weather Section Styles

--- src/fetching.js
+++ src/fetching.js
@@ -1,1 +1,9 @@
 const API_ENDPOINT = "http://api.weatherapi.com/v1";
+
+export async function fetchCurrentWeather() {
+  const res = await fetch(API_ENDPOINT);
+
+  const data = await res.json();
+
+  return data;
+}
