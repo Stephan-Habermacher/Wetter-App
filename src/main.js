aee49aee16d94a2580151cfac35e9cbd57825bab
Stephan-Habermacher <stephan_habermacher@gmx.ch>
2026-03-05 20:39:01 +0100

    Test

--- src/main.js
+++ src/main.js
@@ -2,5 +2,5 @@ import "../styles/main.scss";
 import { renderDetailView } from "./detailView";
 import { renderMainMenu } from "./mainMenu";
 
-//renderDetailView("Sursee");
+renderDetailView("Sursee");
 renderMainMenu(["Sursee", "Luzern", "Rickenbach"]);
